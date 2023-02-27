85b4d1141508959683efe3259073254988f2124a
KhalidElgamous <khalid.elgamous@3wm.ma>
2023-02-27 09:32:10 +0100

    update

--- fallingrain/pipelines.py
+++ fallingrain/pipelines.py
@@ -1,13 +1,10 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import json
 
 
 class FallingrainPipeline:
-    def process_item(self, item, spider):
-        return item
+	
+	def process_item(self, item, spider):
+		print(json.dumps(item, indent=1))
+		return item
